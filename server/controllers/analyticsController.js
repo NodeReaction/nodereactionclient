5e45e35d64a58d7461569ba162e0896adfe4a9c1
Chris Jeon <chrisjwas@gmail.com>
2018-03-16 16:15:49 -0700

    Created response data object

--- server/controllers/analyticsController.js
+++ server/controllers/analyticsController.js
@@ -6,34 +6,44 @@ const analyticsController = {};
 // Given a time range, route, and method, return the avg. response time and # of requests
 // for each 1/60th interval of time in the time range
 analyticsController.responseTime = (req, res, next) => { 
-  let arr = [];
+  let results = [];
+  let cache = {};
   const {route, method, offset, time} = req.params;
-  // offset *= 1000;
-  sql.query(`SELECT AVG(duration), COUNT(*) as numRequests, FROM_UNIXTIME(FLOOR(UNIX_TIMESTAMP(start_timestamp)/(${offset}/60))*(${offset}/60)) as timekey ` + 
-            `FROM transactions ` +
-            `WHERE route='/${route}' and method='${method}' and start_timestamp > '${time}' ` +
-            `GROUP BY UNIX_TIMESTAMP(start_timestamp) DIV (${offset}/60), timekey;`, 
+  // Convert milliseconds to seconds
+  offset *= 1000;
+  // todo: use sqlstring
+  sql.query(
+    `SELECT AVG(duration), COUNT(*) as numRequests, FROM_UNIXTIME(FLOOR(UNIX_TIMESTAMP(start_timestamp) / (${offset} / 60)) * (${offset} / 60)) as timekey ` + 
+    `FROM transactions ` +
+    `WHERE route='/${route}' and method='${method}' and start_timestamp > '${time}' ` +
+    `GROUP BY UNIX_TIMESTAMP(start_timestamp) DIV (${offset} / 60), timekey;`, 
   (err, result) => {
     if (err) return res.send(err);
-    console.log(result);
+    // Make an object from the results
     result.forEach(elem => {
-      let myDate = new Date(elem.timekey);
-      let secondsDate = myDate.getTime() / 1000;
-      console.log(secondsDate);
+      // Change time to seconds
+      timeSec = (new Date(elem.timekey)).getTime() / 1000;
+      // Save avg. time and # of requests
+      cache[timeSec] = {};
+      cache[timeSec]['avgTime'] = elem['AVG(duration)'];
+      cache[timeSec]['numRequests'] = elem['numRequests'];
     });
-
-    currentTime = new Date.getTime() / 1000;
+    // Start time in seconds
+    startRange = (new Date(`${time}`)).getTime() / 1000;
+    // End time in seconds
+    endRange = (new Date).getTime() / 1000;
+    // Check if i is a property in cache
+    // If not, fill the values with 0
+    for (let i = startRange; i < endRange; i += (offset / 60)) {
+      if (!cache.hasOwnProperty(i)) {
+        cache[i] = {};
+        cache[i]['avgTime'] = 0;
+        cache[i]['numRequests'] = 0;
+      }
+    }
+    res.locals.data = cache;
     next();
   });
 }
-//and start_timestamp > '${timeSelector}'
+
 module.exports = analyticsController;
-// I can get the # of millisceonds in 5 minutes, 1 hours, etc
-// time range and interval
-// 5 minutes - 5 seconds
-// 30 minuts - 30 seconds
-// 1 hour = 1 minute
-// 6 hours = 6 minutes
-// 12 hours = 12 minutes
-// 1 day = 24 minutes
-// 3 days = 72 minutes
