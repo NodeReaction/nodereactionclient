0462c5e3aa68807e26211e07a97e5f47e69cb7cc
Chris Jeon <chrisjwas@gmail.com>
2018-03-16 23:36:43 -0700

    Connected route front end to back end

--- server/controllers/routeController.js
+++ server/controllers/routeController.js
@@ -5,8 +5,9 @@ module.exports = {
   getRoutes: function(req, res) {
     let date = "2018-03-15 03:36:49";
     let query = sqlstring.format(
-      `SELECT * FROM transactions
-      WHERE start_timestamp > ?;`,
+      `SELECT route, method, count(transaction_id), avg(duration) FROM transactions
+      WHERE start_timestamp > ?
+      GROUP BY route, method;`,
       [req.params.offset]
     );
 
