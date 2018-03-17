a625a36dc46f600b5b57313b724c32a2d5a70cf5
Chris Jeon <chrisjwas@gmail.com>
2018-03-16 17:15:43 -0700

    Added sql query logic for timeline

--- server/server.js
+++ server/server.js
@@ -39,6 +39,11 @@ app.get('/test/:method/:route/:offset/:time',
   (req, res) => res.send('Success')
 );
 
+app.get('/test2/:method/:route/:offset/:time',
+  analyticsController.timeline,
+  (req, res) => res.send('Success')
+);
+
 // START EXPRESS
 app.listen(PORT, () => {
   console.log(
