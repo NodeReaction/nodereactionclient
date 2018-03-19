23cef88eaa593f2f36db17d40967bfc92a7f3146
Eric Fileti <github.com@ericfileti.com>
2018-03-18 20:53:13 -0700

    improved application selector component

--- client/components/ApplicationSelector.jsx
+++ client/components/ApplicationSelector.jsx
@@ -1,40 +1,57 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 import DropDownMenu from "material-ui/DropDownMenu";
+import FlatButton from "material-ui/FlatButton";
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
 import MenuItem from "material-ui/MenuItem";
 
 export default class ApplicationSelector extends Component {
   constructor(props) {
     super(props);
-    this.state = { value: 1 };
+    this.state = { valueSingle: "3" };
     // this.props.cb(30 * 60000);
-    this.handleChange = this.handleChange.bind(this);
+    this.handleChangeSingle = this.handleChangeSingle.bind(this);
   }
 
-  handleChange(event, index, value) {
-    this.setState({ value });
-    let offset = 0;
-    switch (value) {
-      case 1:
-       
-        break;
-      case 2:
-       
-        break;
-    }
-    // this.props.cb(offset);
-  }
+  handleOpenMenu = () => {
+    this.setState({
+      openMenu: true
+    });
+  };
+
+  handleOnRequestChange = value => {
+    this.setState({
+      openMenu: value
+    });
+  };
+
+  handleChangeSingle = (event, value) => {
+    this.setState({
+      valueSingle: value
+    });
+  };
 
   render() {
     return (
-      <DropDownMenu
-        value={this.state.value}
-        onChange={this.handleChange}
-        openImmediately={false}
+      <IconMenu
+        iconButtonElement={
+          <FlatButton size="medium" label="Applications" primary={true} />
+        }
+        onChange={this.handleChangeSingle}
       >
         <MenuItem value={1} primaryText="Application Name 1" />
         <MenuItem value={2} primaryText="Application Name 2" />
-      </DropDownMenu>
+      </IconMenu>
+
+      // <DropDownMenu
+      //   value={this.state.value}
+      //   onChange={this.handleChange}
+      //   openImmediately={false}
+      // >
+      //   <MenuItem value={1} primaryText="Application Name 1" />
+      //   <MenuItem value={2} primaryText="Application Name 2" />
+      // </DropDownMenu>
     );
   }
 }
