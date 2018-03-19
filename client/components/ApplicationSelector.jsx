61b3dfe83fd83541cb6539fe7ee88246ce6996ed
Eric Fileti <github.com@ericfileti.com>
2018-03-18 21:07:43 -0700

    near final application selector

--- client/components/ApplicationSelector.jsx
+++ client/components/ApplicationSelector.jsx
@@ -5,30 +5,18 @@ import FlatButton from "material-ui/FlatButton";
 import IconButton from 'material-ui/IconButton';
 import IconMenu from 'material-ui/IconMenu';
 import MenuItem from "material-ui/MenuItem";
+import ActionAndroid from 'material-ui/svg-icons/action/android';
 
 export default class ApplicationSelector extends Component {
   constructor(props) {
     super(props);
-    this.state = { valueSingle: "3" };
-    // this.props.cb(30 * 60000);
-    this.handleChangeSingle = this.handleChangeSingle.bind(this);
+    this.state = { currentApplication: "1" };
+    this.handleChangeApplication = this.handleChangeApplication.bind(this);
   }
 
-  handleOpenMenu = () => {
+  handleChangeApplication = (event, value) => {
     this.setState({
-      openMenu: true
-    });
-  };
-
-  handleOnRequestChange = value => {
-    this.setState({
-      openMenu: value
-    });
-  };
-
-  handleChangeSingle = (event, value) => {
-    this.setState({
-      valueSingle: value
+      currentApplication: value
     });
   };
 
@@ -36,9 +24,9 @@ export default class ApplicationSelector extends Component {
     return (
       <IconMenu
         iconButtonElement={
-          <FlatButton size="medium" label="Applications" primary={true} />
+          <FlatButton size="medium" label="Applications" labelPosition="before" primary={true} icon={<ActionAndroid />} />
         }
-        onChange={this.handleChangeSingle}
+        onChange={this.handleChangeApplication}
       >
         <MenuItem value={1} primaryText="Application Name 1" />
         <MenuItem value={2} primaryText="Application Name 2" />
