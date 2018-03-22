1b004abf8d774fd9138fc02cc21f3bce8eecca40
Michael Dalton <modalton@usc.edu>
2018-03-22 15:10:54 -0700

    smoother ui

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -4,7 +4,7 @@ const sqlstring = require("sqlstring");
 module.exports = {
   topFive: function(req, res) {
     let query = sqlstring.format(
-      `select application_id, method, AVG(duration) as avg_duration, COUNT(*) as total_requests from transactions
+      `select route,application_id, method, AVG(duration) as avg_duration, COUNT(*) as total_requests from transactions
 where start_timestamp > ? AND application_id = ?
 group by route,method,application_id
 order by avg_duration desc
@@ -21,8 +21,7 @@ limit 5;`,
     let query = sqlstring.format(
       `select AVG(duration) as avg_duration, COUNT(*) as total_requests from transactions 
 WHERE start_timestamp > ? and application_id = ?;`,
-      [req.params.offset],
-      [req.params.application_id]
+      [req.params.offset, req.params.application_id]
     );
 
     sql.query(query, (err, results, fields) => {
