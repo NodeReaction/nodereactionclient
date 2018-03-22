96118980a0dced01970bf59167bc1a8ca03915a4
Michael Dalton <modalton@usc.edu>
2018-03-21 20:53:01 -0700

    routes refactored to be application based

--- server/controllers/traceController.js
+++ server/controllers/traceController.js
@@ -4,10 +4,10 @@ const sqlstring = require("sqlstring");
 
 function graphData(req, res) {
   let query = sqlstring.format(
-    `SELECT route,method,library,type, AVG(duration) as avg_duration, COUNT(*) as total_requests from traces 
-WHERE start_timestamp > ?
-GROUP BY route,method,library,type;`,
-    [req.params.offset]
+    `SELECT application_id,method,library,type, AVG(duration) as avg_duration, COUNT(*) as total_requests from traces 
+WHERE start_timestamp > ? AND application_id = ?
+GROUP BY route,method,library,type,application_id;`,
+    [req.params.offset, req.params.application_id]
   );
 
   sql.query(query, (err, results, fields) => {
