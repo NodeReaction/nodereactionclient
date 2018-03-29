d75515710a666a77256f048cc04da1142c6bef2a
Chris Jeon <chrisjwas@gmail.com>
2018-03-29 15:46:01 -0700

    Added bcrypt comparison and signup verification

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -8,16 +8,9 @@ const userController = {};
 
 // Creates a new user in the database with bcrypt
 userController.userCreate = (req, res, next) => {
+  console.log('INSIDE CREATE');
   const { email, username, password } = req.body;
   const hashedPassword = bcrypt.hashSync(password, SALT_WORK_FACTOR);
-  console.log(
-    "create user: " +
-      email +
-      " username: " +
-      username +
-      " password: " +
-      password
-  );
   sql.query(
     sqlstring.format(
       "INSERT INTO users (email, username, password) VALUES (?,?,?)",
@@ -28,7 +21,7 @@ userController.userCreate = (req, res, next) => {
         let err = new Error("Invalid credentials or duplicate email/username");
         err.functionName = "userController.createUser";
         err.status = 400;
-        next(err);
+        res.send(err);
       } else {
         res.locals.auth = true;
         res.locals.userId = results.insertId; // Sends back primary key of created user
@@ -38,11 +31,11 @@ userController.userCreate = (req, res, next) => {
   );
 };
 
-// Creates a new user in the database with bcrypt
-userController.userRead = (req, res, next) => {
-  const { id } = req.params;
+userController.signupVerify = (req, res, next) => {
+  const { username, email } = req.body;
+  console.log(req.body);
   sql.query(
-    sqlstring.format("SELECT * from users WHERE id=?", [id]),
+    sqlstring.format("SELECT * from users WHERE username = ? OR email = ?", [username, email]),
     (err, results, fields) => {
       if (err) {
         err = new Error("Invalid credentials");
@@ -50,14 +43,38 @@ userController.userRead = (req, res, next) => {
         err.status = 400;
         next(err);
       } else {
-        //   res.locals.userId = results.insertId;
+        console.log('***RESULTS***\n', results);
+        console.log(results.length);
+        if (results.length > 0) {
+          console.log('INSIDE RESULTS');
+          res.json({"msg": "Pick new username or email"});
+        } else {
+          console.log('INSIDE NEXT');
+          next();
+        }
+      }
+    }
+  );
+};
+
+userController.userRead = (req, res, next) => {
+  const { username, email } = req.params;
+  sql.query(
+    sqlstring.format("SELECT * from users WHERE username = ? OR email = ?", [username, email]),
+    (err, results, fields) => {
+      if (err) {
+        err = new Error("Invalid credentials");
+        err.functionName = "userController.createUser";
+        err.status = 400;
+        next(err);
+      } else {
+          res.locals.userId = results.insertId;
         next();
       }
     }
   );
 };
 
-// Creates a new user in the database with bcrypt
 userController.userUpdate = (req, res, next) => {
   const { id, email, username } = req.body;
   sql.query(
@@ -98,7 +115,7 @@ userController.userDelete = (req, res, next) => {
     }
   );
 };
-// Creates a new user in the database with bcrypt
+
 userController.usersList = (req, res, next) => {
   const { id } = req.params;
   sql.query(
@@ -121,10 +138,11 @@ userController.usersList = (req, res, next) => {
 // Possible extension: For increased security, delay response if error or invalid credentials
 userController.userVerify = (req, res, next) => {
   const { username, password } = req.body;
+  console.log(req.body);
   sql.query(
     sqlstring.format(
-      "SELECT * FROM users WHERE username = ? AND password = ?;",
-      [username, password]
+      "SELECT * FROM users WHERE username = ?;",
+      [username]
     ),
     (err, results, fields) => {
       if (err) {
@@ -135,10 +153,12 @@ userController.userVerify = (req, res, next) => {
       }
       console.log('results =====>' + results);
       if (results.length) {
-        res.locals.auth = true;
-        res.locals.userId = results[0].user_id;
-        // valid credentials
-        next();
+        if (bcrypt.compareSync(password, results[0].password)) {
+          res.locals.auth = true;
+          res.locals.userId = results[0].user_id;
+          // valid credentials
+          next();
+        }
       } else {
         err = new Error("Invalid credentials username: " + username + " password: " + password);
         err.functionName = "userController.verifyUser";
