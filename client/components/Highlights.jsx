dab9e980590aea87105bfd84564a19c7197349d3
Eric Fileti <github.com@ericfileti.com>
2018-04-06 18:12:42 -0700

    front end ui goes right to login

---
+++ client/components/Highlights.jsx
@@ -0,0 +1,15 @@
+
+import React, { Component } from "react";
+
+export default class Highlights extends Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div>
+            <h1>Slide 1 showing hightlights</h1>
+        </div>
+      );
+    }
+  }
